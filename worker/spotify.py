7164ba8e2df30237815bf7cb50debedb888d7ec1
Brian Hines <brian@projectweekend.net>
2015-05-12 05:46:12 -0500

    Add playlist class

--- worker/spotify.py
+++ worker/spotify.py
@@ -14,11 +14,17 @@ class Artist(SpotifyItem):
     def __init__(self, **entries):
         super(Artist, self).__init__(**entries)
 
+    def __repr__(self):
+        return 'Artist: {0}'.format(self.name)
+
 
 class Album(SpotifyItem):
 
     def __init__(self, **entries):
         super(Album, self).__init__(**entries)
+
+    def __repr__(self):
+        return 'Album: {0}'.format(self.name)
 
 
 class Track(SpotifyItem):
@@ -28,6 +34,20 @@ class Track(SpotifyItem):
         self.album = Album(**self.album)
         self.artists = [Artist(**a) for a in self.artists]
 
+    def __repr__(self):
+        return 'Track: {0}'.format(self.name)
+
+
+class Playlist(SpotifyItem):
+
+    def __init__(self, **entries):
+        super(Playlist, self).__init__(**entries)
+        self.url = self.external_urls['spotify']
+        del self.external_urls
+
+    def __repr__(self):
+        return 'Playlist: {0}'.format(self.name)
+
 
 class PlaylistEntry(SpotifyItem):
 
@@ -36,3 +56,6 @@ class PlaylistEntry(SpotifyItem):
         self.track = Track(**self.track)
         self.added_at = datetime.strptime(self.added_at, '%Y-%m-%dT%H:%M:%SZ')
         self.added_date = self.added_at.date()
+
+    def __repr__(self):
+        return 'PlaylistEntry: {0}'.format(self.track.name)
