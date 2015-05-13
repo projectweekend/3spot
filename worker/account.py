ae1017b0b18f594bd76cb647edbefdb563b67a67
Brian Hines <brian@projectweekend.net>
2015-05-13 05:46:36 -0500

    Working on Account class

--- worker/account.py
+++ worker/account.py
@@ -1,33 +1,52 @@
 from datetime import date
 import spotipy
-from spotify import Playlist, PlaylistEntry
+from boto.dynamodb2.table import Table
+from spotify_models import Playlist, PlaylistEntry
+from spotify_api import new_access_token
 
 
 class Account(object):
 
-    def __init__(self, username, access_token, refresh_token):
+    def __init__(self, username, playlist_id):
         self._username = username
-        self._access_token = access_token
-        self._refresh_token = refresh_token
-        self._client = spotipy.Spotify(auth=self._access_token)
+        self._playlist_id = playlist_id
+        self._model = self._database_model()
+        self._spotify = self._spotify_client()
 
-    def playlist(self, playlist_id):
-        result = self._client.user_playlist(
+    def __repr__(self):
+        return "<Account: {0} - {1}>".format(self._username, self._playlist_id)
+
+    def _database_model(self):
+        accounts = Table('accounts')
+        return accounts.get_item(
+            spotify_username=self._username,
+            spotify_playlist_id=self._playlist_id)
+
+    def _spotify_client(self):
+        self._refresh_access_token()
+        return spotipy.Spotify(auth=self._model['access_token'])
+
+    def _refresh_access_token(self):
+        self._model['access_token'] = new_access_token(self._model['refresh_token'])
+        self._model.save()
+
+    def playlist(self):
+        result = self._spotify.user_playlist(
             self._username,
-            playlist_id,
+            self._model['spotify_playlist_id'],
             fields="name,external_urls")
         return Playlist(**result)
 
-    def todays_track(self, playlist_id):
+    def todays_track(self):
         def was_added_today(entry):
             return date.isoformat(date.today()) == date.isoformat(entry.added_date)
 
         offset = 0
         todays_track = None
         while True:
-            result = self._client.user_playlist_tracks(
+            result = self._spotify.user_playlist_tracks(
                 self._username,
-                playlist_id,
+                self._model['spotify_playlist_id'],
                 offset=offset)
             entries = [PlaylistEntry(**i) for i in result['items']]
             try:
