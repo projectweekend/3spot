4bc9d1b51cd735c366edce81cd4e36e2eca904c7
Brian Hines <brian@projectweekend.net>
2015-05-16 09:26:00 -0500

    Add echo nest to artist model

--- worker/models/spotify_artist.py
+++ worker/models/spotify_artist.py
@@ -1,10 +1,17 @@
 from spotify_item import SpotifyItem
+from pyechonest import config
+from pyechonest import artist
+from worker.config import ECHO_NEST_API_KEY
+
+
+config.ECHO_NEST_API_KEY = ECHO_NEST_API_KEY
 
 
 class Artist(SpotifyItem):
 
     def __init__(self, **entries):
         super(Artist, self).__init__(**entries)
+        self.echonest = artist.Artist(self.uri)
 
     def __repr__(self):
         return '<Artist: {0}>'.format(self.name)
