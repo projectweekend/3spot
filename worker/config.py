48d9c43ec9fc078c1ae704d1b2850bcb120c10c2
Brian Hines <brian@projectweekend.net>
2015-05-20 05:35:34 -0500

    More config

--- worker/config.py
+++ worker/config.py
@@ -7,6 +7,10 @@ SLEEP_TIME = os.getenv('SLEEP_TIME', 60)
 
 PROD_LOGGER = os.getenv('PROD_LOGGER', False)
 
+SQS_QUEUE_NAME = os.getenv('SQS_QUEUE_NAME', 'song-feed-playlists-to-process')
+
+SQS_REGION = os.getenv('SQS_REGION', 'us-east-1')
+
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 assert SPOTIFY_CLIENT_ID
 
